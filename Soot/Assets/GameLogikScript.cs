45db93e491693be6f9b39fb436b5fac39ed45e1f
Pascal Mangold <82329799+ArangoJango@users.noreply.github.com>
2024-02-16 10:29:27 +0100

    Implementation Level 2
    + Levelswitch 1 / 2
    + Level 2 Plattformen
    + UI Info Text

---
+++ Soot/Assets/GameLogikScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogikScript : MonoBehaviour
+{
+    public GameObject Star1, Star2, Star3, Star4, Star5, Star6, Star7, Star8, Star9, Star10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
